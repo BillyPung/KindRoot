52d0eb17e6438dd4848dff65664a993cafdb855f
Patoooo <sy2470@nyu.edu>
2023-02-05 01:22:46 +0800

    :+1:  加了一个三秒延迟后冲刺吃绳子的虫子

---
+++ KindRoot/Assets/Scripts/TriggerWorm.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerWorm : MonoBehaviour
+{
+    public bool triggered = false;
+    public bool canMove = false;
+    public bool initialMove;
+    public float beforeFlyTime;
+    public Vector2 startPos;
+    public Rigidbody2D rig;
+    
+    public float speed = 100f;
+    public float moveTime;
+    private float _moveTimer;
+    
+    public float initialMoveTime;
+    private float _initialMoveTimer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = (Vector2)transform.position;
+        rig = GetComponent<Rigidbody2D>();
+        _moveTimer = moveTime;
+        _initialMoveTimer = initialMoveTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggered)
+        {
+            StartCoroutine(Fly());
+        }
+
+        if (initialMove)
+        {
+            if (_initialMoveTimer >= 0)
+            {
+                _initialMoveTimer -= Time.deltaTime;
+                rig.velocity = Vector2.right * Time.deltaTime * 20f;
+            }
+            else
+            {
+                _initialMoveTimer = initialMoveTime;
+                rig.velocity = Vector2.zero;
+                initialMove = false;
+            }
+        }
+        
+        if (canMove)
+        {
+            if (_moveTimer >= 0)
+            {
+                _moveTimer -= Time.deltaTime;
+                rig.velocity = Vector2.right * Time.deltaTime * speed;
+                speed += 3000f * Time.deltaTime;
+            }
+            else if(_moveTimer < 0)
+            {
+                _moveTimer = moveTime;
+                canMove = false;
+                rig.velocity = Vector2.zero;
+                transform.position = startPos;
+                speed = 300f;
+                initialMove = true;
+            }
+        }
+    }
+
+    IEnumerator Fly()
+    {
+        yield return new WaitForSeconds(beforeFlyTime);
+        canMove = true;
+        triggered = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (triggered)
+        {
+            if(col.gameObject.name == "Rope_1" || col.gameObject.name == "Rope_2")
+            {
+                col.gameObject.GetComponent<NewRopeScript>().maxDist -= 1.5f;
+            }
+        }
+    }
+}
---
+++ KindRoot/Assets/Scripts/WormTrigger.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormTrigger : MonoBehaviour
+{
+    public bool startCountDown = false;
+    public bool canTrigger;
+
+    public float noTriggerTime;
+    private float _noTriggerTimer;
+
+    public TriggerWorm tgw;
+        
+        // Start is called before the first frame update
+    void Start()
+    {
+        _noTriggerTimer = noTriggerTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startCountDown)
+        {
+            if (_noTriggerTimer < 0)
+            {
+                _noTriggerTimer = noTriggerTime;
+                canTrigger = true;
+                startCountDown = false;
+            }
+            else
+            {
+                _noTriggerTimer -= Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player_1" || col.gameObject.name == "Player_2")
+        {
+            if (canTrigger)
+            {
+                startCountDown = true;
+                tgw.triggered = true;
+            }
+        }
+    }
+}
