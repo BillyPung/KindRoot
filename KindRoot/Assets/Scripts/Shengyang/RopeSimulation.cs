aaaa21e55ceb227b69bfcc0837ccd4dcba3ca2af
Billy Peng <62238461+BillyPung@users.noreply.github.com>
2023-02-04 00:59:24 -0500

    doorScript

---
+++ KindRoot/Assets/Scripts/Shengyang/RopeSimulation.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class RopeSimulation : MonoBehaviour
+{
+    // The prefab of the dot that will represent the rope
+    public GameObject dotPrefab;
+    public GameObject player;
+    
+}
