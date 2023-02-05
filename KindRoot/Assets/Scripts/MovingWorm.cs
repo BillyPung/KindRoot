edcfd68debbd337f4af63c3677a10e7ebf53932d
Xiao-Peng-Sophie <xp2022@nyu.edu>
2023-02-05 11:37:19 -0500

    blue worm animation

--- KindRoot/Assets/Scripts/MovingWorm.cs
+++ KindRoot/Assets/Scripts/MovingWorm.cs
@@ -4,32 +4,45 @@ using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using UnityEngine;
 
+
 public class MovingWorm : MonoBehaviour
 {
     public GameObject leftBound;
+    public SpriteRenderer sp;
     public GameObject rightBound;
     public Rigidbody2D rig;
     public float speed;
     private AudioSource audioSource;
     public AudioClip biteSound;
+    public bool flip = false;
+
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         rig = GetComponent<Rigidbody2D>();
         rig.velocity = new Vector2(speed, 0);
+        sp = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+
         if (transform.position.x > rightBound.transform.position.x || transform.position.x < leftBound.transform.position.x)
         {
             speed *= -1;
+
+            flip = !flip;
+
+
         }
-        
+
+        sp.flipX = flip;
         rig.velocity = new Vector2(speed, 0);
+
+
     }
 
     private void OnTriggerEnter2D(Collider2D col)
